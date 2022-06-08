44cface51da3573a01459238036b1bb2e21017e8
David Celis <me@davidcel.is>
2022-06-07 18:27:26 -0700

    Add an example of a deprecated field

--- app/graphql/objects/houseware.rb
+++ app/graphql/objects/houseware.rb
@@ -9,4 +9,9 @@ class Objects::Houseware < Objects::BaseObject
   description "Houseware is a type of general-purpose furniture item in Animal Crossing."
 
   field :category, String, null: false, description: "A shared category of which this item is a member.", method: :tag
+
+  field :size, String, null: false, description: "The size of the item, formatted as lenth x width.", deprecation_reason: "This field was added before a robust `Item` interface was extracted. You should prefer the `length` and `width` fields provided by that interface instead."
+  def size
+    [sprintf("%g", object.length), sprintf("%g", object.width)].join("x")
+  end
 end
